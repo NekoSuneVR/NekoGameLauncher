1efa76a26e9b3c726558bf93bfa8038dbb5e3042
NekoSuneVR <Chisdeal2014@hotmail.com>
2026-09-01 08:57:03 +0100

    feat: add custom dark titlebar window controls

--- src/NekoGameLauncher/MainWindow.xaml.cs
+++ src/NekoGameLauncher/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 using NekoGameLauncher.ViewModels;
 using System.Windows;
+using System.Windows.Input;
 
 namespace NekoGameLauncher;
 
@@ -13,4 +14,30 @@ public partial class MainWindow : Window
         DataContext = _viewModel;
         Loaded += async (_, _) => await _viewModel.InitializeAsync();
     }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton != MouseButton.Left) return;
+
+        if (e.ClickCount == 2)
+        {
+            ToggleMaximize();
+            return;
+        }
+
+        try { DragMove(); }
+        catch (InvalidOperationException) { }
+    }
+
+    private void Minimize_Click(object sender, RoutedEventArgs e)
+        => WindowState = WindowState.Minimized;
+
+    private void Maximize_Click(object sender, RoutedEventArgs e)
+        => ToggleMaximize();
+
+    private void Close_Click(object sender, RoutedEventArgs e)
+        => Close();
+
+    private void ToggleMaximize()
+        => WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
 }
